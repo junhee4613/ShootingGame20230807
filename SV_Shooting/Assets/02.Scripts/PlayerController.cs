6b6c5c7f888adf146dd609773e6417d19d31f908
junhee4613 <junhee000123@naver.com>
2023-08-07 14:39:32 +0900

    슈팅게임 1일차

---
+++ SV_Shooting/Assets/02.Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    public Vector2 moveInput;
+
+    public Transform topLeft;
+    public Transform bottomRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = moveInput * moveSpeed;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, topLeft.position.x, bottomRight.position.x),
+            Mathf.Clamp(transform.position.y, bottomRight.position.y, topLeft.position.y), transform.position.z);
+    }
+}
